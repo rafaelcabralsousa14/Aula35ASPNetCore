5476be079feade2e92f7fbb43154487d3d5a6142
unknown <rafacs1404@gmail.com>
2020-07-09 09:48:47 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aula35AspNETCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
